41be54276e9c84ce5a454969dc205ceeadda36c5
Raksha <rakshapoojary906@mail.com>
2026-03-22 15:50:06 +0530

    Loading-Page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading";
 import Landing from "./pages/Landing";
 import Game from "./pages/Game";
 import Multiplayer from "./pages/Multiplayer";
@@ -6,6 +8,25 @@ import MultiplayerGame from "./pages/MultiplayerGame";
 import "./styles/Game.css";
 
 function App() {
+    const [backendReady, setBackendReady] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetch("http://localhost:5000/api/user/health")
+                .then(() => {
+                    setBackendReady(true);
+                    clearInterval(interval);
+                })
+                .catch(() => { });
+        }, 3000);
+        return () => clearInterval(interval);
+    }, []);
+
+    if (!backendReady) {
+        return <Loading />;
+    }
+
+
     return (
         <BrowserRouter>
             <Routes>
---
+++ FrontEnd/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+export default function Loading(){
+    return (
+        <div className="loading-container">
+            <div className="loader-card">
+                <div className="spinner">
+                    <h2>Waking Up server...</h2>
+                    <p>This may take 10-40sec</p>
+                </div>
+            </div>
+        </div>
+    );
+}
