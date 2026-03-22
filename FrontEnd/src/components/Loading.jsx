e720cd701318a9e6cc812917887ec270e26a34b1
Raksha <rakshapoojary906@mail.com>
2026-03-22 16:41:48 +0530

    fix loading page

--- FrontEnd/src/components/Loading.jsx
+++ FrontEnd/src/components/Loading.jsx
@@ -2,10 +2,9 @@ export default function Loading(){
     return (
         <div className="loading-container">
             <div className="loader-card">
-                <div className="spinner">
-                    <h2>Waking Up server...</h2>
-                    <p>This may take 10-40sec</p>
-                </div>
+                <div className="spinner"></div>
+                <h2>Waking Up server...</h2>
+                <p>This may take 10-40sec</p>
             </div>
         </div>
     );
